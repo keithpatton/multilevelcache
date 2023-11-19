29db181649a2db96339f799703ca63f6cc6b8ac4
Keith Patton <keith.patton@outlook.com>
2023-11-19 20:30:27 +1300

    add test for background refresh

--- src/MultiLevelCache/MultiLevelCacheApi.Tests/IntegrationTests.cs
+++ src/MultiLevelCache/MultiLevelCacheApi.Tests/IntegrationTests.cs
@@ -1,15 +1,22 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
 using MultiLevelCacheApi;
+using MultiLevelCacheApi.Options;
 using Newtonsoft.Json;
 
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
         {
-            // Customize services for testing as needed
+            services.Configure<CacheOptions>(options =>
+            {
+                // reduce time for testing background refresh is working
+                options.StaleAfterDefault = MultiLevelCacheApi.Tests.WeatherForecastControllerIntegrationTests.StaleAfterDefault;
+            });
         });
     }
 }
@@ -19,6 +26,8 @@ namespace MultiLevelCacheApi.Tests
     [TestClass]
     public class WeatherForecastControllerIntegrationTests
     {
+
+        public static TimeSpan StaleAfterDefault = TimeSpan.FromSeconds(5);
         private readonly HttpClient _client;
 
         public WeatherForecastControllerIntegrationTests()
@@ -65,7 +74,36 @@ namespace MultiLevelCacheApi.Tests
             Assert.AreNotEqual(JsonConvert.SerializeObject(firstForecasts), JsonConvert.SerializeObject(secondForecasts));
         }
 
+        [TestMethod]
+        public async Task GetAsync_BackgroundRefresh_Success()
+        {
+            // Initial request to trigger caching
+            await _client.GetAsync("/WeatherForecast/GetForecast");
+
+            // Wait for the data to become stale
+            await Task.Delay(StaleAfterDefault);
+
+            // Polling loop
+            IEnumerable<WeatherForecast> lastForecasts = Enumerable.Empty<WeatherForecast>();
+            IEnumerable<WeatherForecast> forecasts = Enumerable.Empty<WeatherForecast>();
+            for (int i = 0; i < 60; i++)
+            {
+                var response = await _client.GetAsync("/WeatherForecast/GetForecast");
+                var data = await response.Content.ReadAsStringAsync();
+                forecasts = JsonConvert.DeserializeObject<IEnumerable<WeatherForecast>>(data)!;
+
+                if (lastForecasts != null && (JsonConvert.SerializeObject(forecasts) != JsonConvert.SerializeObject(lastForecasts)))
+                {
+                    // Data has been background refreshed!
+                    break;
+                }
+                lastForecasts = forecasts!;
+                await Task.Delay(1000);
+            }
+
+            Assert.AreNotEqual(JsonConvert.SerializeObject(lastForecasts), JsonConvert.SerializeObject(forecasts));
+        }
+
     }
 
 }
-
