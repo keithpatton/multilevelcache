de9a0a0f4c01467546ec5269202f156a7b0bb1a3
Keith Patton <Keith.Patton@serko.com>
2023-11-20 13:13:36 +1300

    add polly retry for redis exceptions in cache service

--- src/MultiLevelCache/MultiLevelCacheApi/Services/CacheService.cs
+++ src/MultiLevelCache/MultiLevelCacheApi/Services/CacheService.cs
@@ -2,6 +2,9 @@
 using Microsoft.Extensions.Options;
 using MultiLevelCacheApi.Abstractions;
 using MultiLevelCacheApi.Options;
+using Polly;
+using Polly.Retry;
+using StackExchange.Redis;
 
 namespace MultiLevelCacheApi.Services
 {
@@ -15,11 +18,27 @@ namespace MultiLevelCacheApi.Services
     {
         private ICacheStack _cacheStack;
         private CacheOptions _cacheOptions;
+        private readonly AsyncRetryPolicy _retryPolicy;
 
         public CacheService(ICacheStack cacheStack, IOptions<CacheOptions> options)
         {
             _cacheStack = cacheStack;
             _cacheOptions = options.Value;
+
+            // retry for Redis specific exceptions only coming from the Redis Cache Layer
+            // note: be careful as the GetOrSetAsync covers the ValueFactory 
+            // this  could raise wide range of exceptions and you may not want this to be retried
+            _retryPolicy = Policy
+                .Handle<RedisException>()
+                .Or<RedisTimeoutException>()
+                .WaitAndRetryAsync(
+                    3, // Number of retries
+                    retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), // Exponential back-off
+                    onRetry: (exception, timeSpan, retryCount, context) =>
+                    {
+                        // Optional: Log the retry attempt
+                    }
+                );
         }
 
         public CacheSettings GetCacheSettingsDefault()
@@ -27,14 +46,21 @@ namespace MultiLevelCacheApi.Services
             return new CacheSettings(_cacheOptions.TimeToLiveDefault, _cacheOptions.StaleAfterDefault);
         }
 
-        public ValueTask EvictAsync(string cacheKey)
+        public async ValueTask EvictAsync(string cacheKey)
         {
-            return _cacheStack.EvictAsync(CreateCacheKey(cacheKey));
+            await _retryPolicy.ExecuteAsync(async () =>
+            {
+                await _cacheStack.EvictAsync(CreateCacheKey(cacheKey));
+            });
+           
         }
 
-        public ValueTask<T> GetOrSetAsync<T>(string cacheKey, Func<T, Task<T>> valueFactory, CacheSettings settings)
+        public async ValueTask<T> GetOrSetAsync<T>(string cacheKey, Func<T, Task<T>> valueFactory, CacheSettings settings)
         {
-            return _cacheStack.GetOrSetAsync<T>(CreateCacheKey(cacheKey), valueFactory, settings);
+            return await _retryPolicy.ExecuteAsync(async () =>
+            {
+                return await _cacheStack.GetOrSetAsync<T>(CreateCacheKey(cacheKey), valueFactory, settings);
+            });
         }
 
         private string CreateCacheKey(string cacheKey)
